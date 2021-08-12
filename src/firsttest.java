677097b5d2fa897332ad3e3f755d1efd686c1b01
kanchoti <kanikachaudhary97.kc@gmail.com>
2021-08-13 00:36:37 +0530

    first commit

---
+++ src/firsttest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firsttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Webdriver driver= new ChromeDriver();
+	}
+
+}
